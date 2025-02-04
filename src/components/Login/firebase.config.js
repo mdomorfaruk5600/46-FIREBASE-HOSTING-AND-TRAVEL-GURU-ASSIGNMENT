dfc87ab4d187ab3f5d34ed34d75994a8879478a1
mdomorfaruk5600 <md.omorfaruk5600@gmail.com>
2025-02-04 09:55:51 +0600

    Update firebase.config.js

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,17 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDngUjV2A6UJ53YXEaE9dsg1bSiO8tMn1w",
 
-  authDomain: "fire-auth-775e1.firebaseapp.com",
-
-  projectId: "fire-auth-775e1",
-
-  storageBucket: "fire-auth-775e1.firebasestorage.app",
-
-  messagingSenderId: "685180112844",
-
-  appId: "1:685180112844:web:0d2938a7258dcd3cc55769",
-
-  measurementId: "G-R0QTP068SM"
 
   };
   
